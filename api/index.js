2db3b71623a7b3bff452617ed2f26a016797e3e5
CamNav <caminavinta14@gmail.com>
2024-06-04 16:41:38 -0500

    feat(api): create test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose
@@ -14,6 +15,8 @@ then(()=>{
 
 const app = express();
 app.listen(3000, () => 
-    {console.log('Server is running')
-
+    {console.log('Server is running on port 3000')
 });
+
+app.use("/api/user", userRoutes);
+
