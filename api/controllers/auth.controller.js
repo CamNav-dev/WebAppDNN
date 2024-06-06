0ecbd52c68f80f44bdc6c75776ad1113cadb4f3b
CamNav <caminavinta14@gmail.com>
2024-06-06 12:09:58 -0500

    feat(api): add error handling middleware and function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-export const signup = async(req,res) =>{
+import { errorHandler } from '../utils/error.js';
+
+export const signup = async(req, res, next) =>{
     const {username, email, password} = req.body;
     {/* Password encypted */}
     const hashPassword = bcrypt.hashSync(password, 10);
@@ -9,6 +11,11 @@ export const signup = async(req,res) =>{
         email,
         password: hashPassword
     });
-    await newUser.save();
-    res.status(201).json({message: 'User created successfully'});
-}
+
+    try{ 
+        await newUser.save();
+        res.status(200).json({message: 'User created successfully'});
+    }   catch(error){
+        next(error);
+    }
+};
