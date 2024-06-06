6652c833880eec8b5cf7977a179d39e253aafc41
CamNav <caminavinta14@gmail.com>
2024-06-06 08:45:08 -0500

    feat(api): create signup API routh

--- api/index.js
+++ api/index.js
@@ -2,21 +2,23 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
-mongoose
-.connect(process.env.MONGO).
-then(()=>{
-    console.log('Connected to MongoDB');
+const mongoURI = process.env.MONGO;
+
+mongoose.connect(mongoURI, {
+  dbName: 'DNNWebAppDB', 
 })
-.catch((err)=>{
-    console.log(err);
-});
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
 
 const app = express();
-app.listen(3000, () => 
-    {console.log('Server is running on port 3000')
+app.use(express.json());
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
 app.use("/api/user", userRoutes);
-
+app.use("/api/auth", authRoutes);
