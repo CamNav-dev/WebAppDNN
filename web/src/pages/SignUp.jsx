e548c38bad57a95f8e4b4b31343b4ecf37d16676
CamNav <caminavinta14@gmail.com>
2024-06-10 23:14:59 -0500

    feat(web): add complete signup page functionality

--- web/src/pages/SignUp.jsx
+++ web/src/pages/SignUp.jsx
@@ -1,7 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    setLoading(true);
+    setError(false);
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+    setLoading(false);
+    if (data.success === false) {
+      setError(true);
+      return;
+    }
+  } catch (error) {
+    setLoading(false);
+    setError(true);
+  }
+};
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <div
@@ -12,7 +45,7 @@ export default function SignUp() {
           <h2 className="text-2xl font-bold">Crear una cuenta</h2>
           <p className="text-gray-500">¿Eres nuevo en ...?</p>
         </div>
-        <form>
+        <form onSubmit = {handleSubmit}>
           <div className="mb-4">
             <label
               className="block text-gray-700 font-bold mb-2"
@@ -22,10 +55,11 @@ export default function SignUp() {
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
+              id="username"
               type="text"
-              placeholder="Enter your name"
+              placeholder=""
               required
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -39,8 +73,9 @@ export default function SignUp() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="email"
               type="email"
-              placeholder="Enter your email"
+              placeholder=""
               required
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -54,16 +89,17 @@ export default function SignUp() {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              placeholder="Enter your password"
+              placeholder=""
               required
+              onChange={handleChange}
             />
           </div>
           <div className="flex items-center justify-center">
             <button
               className="mt-4 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-1/2"
-              type="button"
+              disabled={loading}
             >
-              Crear usuario
+              {loading ? 'Loading...' : 'Crear Usuario'}
             </button>
           </div>
           <div className=" mt-4 text-center text-gray-500">
@@ -72,6 +108,7 @@ export default function SignUp() {
               Iniciar sesión
             </Link>
           </div>
+          <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
         </form>
       </div>
     </div>
