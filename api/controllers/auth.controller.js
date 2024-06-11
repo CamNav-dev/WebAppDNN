9c8359f567186b355e084ede6f268ccb2de15363
CamNav <caminavinta14@gmail.com>
2024-06-11 12:11:40 -0500

    feat(api): add signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
-
+import jwt from 'jsonwebtoken';
 export const signup = async(req, res, next) =>{
     const {username, email, password} = req.body;
     {/* Password encypted */}
@@ -15,6 +15,21 @@ export const signup = async(req, res, next) =>{
     try{ 
         await newUser.save();
         res.status(200).json({message: 'User created successfully'});
+    }   catch(error){
+        next(error);
+    }
+};
+
+export const signin = async(req, res, next) =>{
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user) return next(errorHandler(404, 'User not found'));
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+        if(!isPasswordCorrect) return next(errorHandler(401, 'Invalid password'));
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        const {password: hashPassword, ...rest} = user._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(user);
     }   catch(error){
         next(error);
     }
