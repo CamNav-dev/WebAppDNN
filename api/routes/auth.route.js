1fd81bc640f00f97925fa05d8688c12e7694127c
delilux <142470442+delilux@users.noreply.github.com>
2024-09-12 14:03:21 -0500

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signin, signup, deleteUser, updateUser } from '../controllers/auth.controller.js';
+import { signin, signup, deleteUser, updateUser, payment } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 router.post("/signup",signup)
 router.post("/signin", signin)
 router.delete('/delete/:id', deleteUser);
 router.put('/update/:id', updateUser);
+router.post("/payment", payment)
 
 export default router;
